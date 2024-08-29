6911a2854bfd4fde5efeb75ebfaf4ab12f602af4
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-08-30 01:48:39 +0530

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import NavbarItems from "./NavbarItems";
 import Link from "next/link";
 
 export default function Navbar () {
     return(
-        <div className="w-full flex justify-between items-start p-4">
-            <Link href={"/"}><Image className="h-[40px] w-auto" src={"/assets/FLNT.png"} height={100} width={200} alt="FLNT" /></Link>
+        <div className="w-full flex justify-between items-start p-4 fixed top-0 z-[1]">
+            <Link href={"/"}><Image className="h-[40px] w-auto max-sm:h-[30px]" src={"/assets/FLNT.png"} height={100} width={200} alt="FLNT" /></Link>
             <NavbarItems />
         </div>
     )
--- src/components/NavbarItems.tsx
+++ src/components/NavbarItems.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
 
 export default function NavbarItems() {
+  const [menu, setMenu] = useState(true);
+  const { scrollY } = useScroll();
+  let flexStyle;
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 20) {
+      setMenu(false);
+      flexStyle = "flex justify-between items-center"
+    } else {
+      setMenu(true);
+      flexStyle = ""
+    }
+  });
+
   return (
     <div className="flex gap-9 text-[15px] font-semibold">
-      <ul>
+      <ul className={`${flexStyle}`}>
         <li>
           <Link href={"/"}>HOME</Link>
         </li>
@@ -17,23 +33,29 @@ export default function NavbarItems() {
           <Link href={"/contact"}>CONTACT US</Link>
         </li>
       </ul>
-
-      <ul>
-        <li>
-          <Link href={"/"}>EMAIL</Link>
-        </li>
-        <li>
-          <Link href={"/"}>TWITTER</Link>
-        </li>
-        <li>
-          <Link href={"/"}>LINKEDIN</Link>
-        </li>
-      </ul>
-
-      <button className="relative border-2 h-11 px-4 group overflow-hidden">
-        <p className="group-hover:text-primary transition-all">BOOK A CALL</p>
-        <div className="absolute top-[100%] left-0 bg-secondary h-full w-full group-hover:top-0 transition-all -z-10"></div>
-      </button>
+      {menu ? (
+        <>
+          <ul>
+            <li>
+              <Link href={"/"}>EMAIL</Link>
+            </li>
+            <li>
+              <Link href={"/"}>TWITTER</Link>
+            </li>
+            <li>
+              <Link href={"/"}>LINKEDIN</Link>
+            </li>
+          </ul>
+          <button className="relative border-2 h-11 px-4 group overflow-hidden">
+            <p className="group-hover:text-primary transition-all">
+              BOOK A CALL
+            </p>
+            <div className="absolute top-[100%] left-0 bg-secondary h-full w-full group-hover:top-0 transition-all -z-10"></div>
+          </button>{" "}
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
