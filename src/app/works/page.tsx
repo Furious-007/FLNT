93f0e41fb704b22f68eb790944df6cea2450ee3e
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-08-30 00:26:30 +0530

    initial commit

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,14 +1,36 @@
-'use client'
+"use client";
 
 import TransitionPage from "@/components/transitionPage";
 import Link from "next/link";
+import { motion, cubicBezier } from "framer-motion";
 
 export default function Works() {
+  const textVar = {
+    initial: {
+      top: 100,
+      clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)",
+    },
+    whileInView: {
+      top: 0,
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      transition: {
+        ease: cubicBezier(0, 0.55, 0.45, 1),
+        duration: 0.75,
+        delay: 1.05,
+      },
+    },
+  };
   return (
     <div>
       <TransitionPage />
-      This is Works Page
-      <Link href={"/"}>HOME</Link>
+      <motion.p
+        className="relative text-[120px]"
+        variants={textVar}
+        initial="initial"
+        whileInView="whileInView"
+      >
+        WORKS
+      </motion.p>
     </div>
   );
 }
